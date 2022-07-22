4fe937087c737fbea1c69ae032c5906eaaaf4e09
nigel <>
2022-07-22 18:05:13 +0200

    create CalendarUtils add functions to generate dates for every day of the current week and for the current month

---
+++ src/utils/CalendarUtils.js
@@ -0,0 +1,39 @@
+let CalendarUtils = {
+  addDays: (date, days) => {
+    let result = new Date(date);
+    result.setDate(result.getDate() + days);
+    return result;
+  },
+
+  /**
+   *
+   * @param date
+   * @returns returns a list of days in this week
+   */
+  getWeekDaysList: (date)=>{
+    let day = date.getDay();
+    let result = [];
+    for(let dayOfWeek = 0; dayOfWeek < 7; dayOfWeek++){
+      result.push(CalendarUtils.addDays(date, dayOfWeek - day + 1))
+    }
+    return result;
+  },
+
+  /**
+   *
+   * @param date
+   * @returns returns a list of all the weeks in the month, a week is a list of all the days in the week, so the result is a list of lists of days
+   */
+  getMonthWeeksList: (date) => {
+    let dayOfMonth = date.getDate();
+    let dateRunner = CalendarUtils.addDays(date, -date.getDate() + 1); //initialize to first day of month
+    let result = [];
+    while(dateRunner.getMonth() === date.getMonth()){
+      result.push(CalendarUtils.getWeekDaysList(dateRunner)); // add the list of weekdays to the result list if dateRunner in current month
+      dateRunner = CalendarUtils.addDays(dateRunner, 7); // set dateRunner to the next week
+    }
+    return result;
+  },
+}
+
+export default CalendarUtils;
