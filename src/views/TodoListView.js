b71325b7ab01ab3f16e01c0a54fbcb110e475d0e
nigel <>
2022-07-27 08:10:46 +0200

    add todoform inside a modal to todolistview

--- src/views/TodoListView.js
+++ src/views/TodoListView.js
@@ -1,15 +1,32 @@
 import TodoList from "../components/TodoList";
 import { useDispatch } from 'react-redux';
 import {add} from "../store/todoListSlice";
+import {useState} from "react";
+import Modal from "../components/ui/Modal";
+import TodoItemObj from "../model/TodoItemObj";
+import TodoForm from "../components/TodoForm";
 
 function TodoListView(){
+  const [modalActive, setModalActive] = useState(false)
+
+  const onOutsideClickHandler = () => {
+    setModalActive(false);
+  }
+
   const dispatch = useDispatch();
-  const clickHandler = ()=>{dispatch(add())};
+  const clickHandler = ()=>{
+    setModalActive(true);
+    // let item = new TodoItemObj("ajadrgnköa", "kjadnf", "jagrn", "kjrangk")
+    // dispatch(add(item))
+  };
 
   return <div>
     <h1>Todo List</h1>
     <TodoList />
     <button onClick={clickHandler}>Add</button>
+    <Modal active={modalActive} onClickOutside={onOutsideClickHandler}>
+      <TodoForm afterSubmit={onOutsideClickHandler} />
+    </Modal>
   </div>
 }
 
