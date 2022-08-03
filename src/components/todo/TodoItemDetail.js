a1fa4d92cc87f70064f9d58a70ac750050e0374c
nigel <>
2022-08-03 19:55:57 +0200

    implement setitemdone function

--- src/components/todo/TodoItemDetail.js
+++ src/components/todo/TodoItemDetail.js
@@ -1,12 +1,17 @@
+import { useDispatch } from 'react-redux';
+import {addOrUpdate} from "../../store/todoDictDtoSlice";
+
 function TodoItemDetail(props){
   let item = props.item;
+  const dispatch = useDispatch();
 
   if(props.item === undefined){
     return;
   }
 
   const setItemDone = () => {
-
+    item.done = true;
+    dispatch(addOrUpdate(item.getDto()));
   }
 
   return <div className="flex flex-col rounded shadow m-2 bg-blue-400">
@@ -17,7 +22,7 @@ function TodoItemDetail(props){
     <div className="m-2 p-2 rounded bg-blue-200">{item.description}</div>
     <div className="flex flex-row">
       <button className="m-2 p-2 rounded shadow bg-blue-200">Del</button>
-      <button className="m-2 p-2 rounded shadow bg-blue-200">Done</button>
+      <button className="m-2 p-2 rounded shadow bg-blue-200" onClick={setItemDone}>Done</button>
     </div>
   </div>
 }
