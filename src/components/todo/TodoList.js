9772d1e32e26b765d77153e570f6f38c3ef17211
nigel <>
2022-08-04 08:00:50 +0200

    in todolist view, sort items by date

--- src/components/todo/TodoList.js
+++ src/components/todo/TodoList.js
@@ -2,14 +2,31 @@ import { useSelector } from 'react-redux';
 
 import TodoItemObj from "../../model/TodoItemObj";
 import TodoItem from "./TodoItem";
+import CalendarUtils from "../../utils/CalendarUtils";
 
 function TodoList() {
   const todoList = useSelector(state=>Object.keys(state.todoDictDto).map((key)=>TodoItemObj.fromDto(state.todoDictDto[key])));
 
+  const todoDateMap = {}
+  todoList.forEach((item) => {
+    let dateString = CalendarUtils.formatDate(item.date);
+    if(todoDateMap[dateString] === undefined){
+      todoDateMap[dateString] = [];
+    }
+    todoDateMap[dateString].push(item)
+  })
+
+  const jsxContent = [];
+  Object.keys(todoDateMap).forEach((dateKey)=>{
+    jsxContent.push(<p className="">{dateKey}</p>)
+    todoDateMap[dateKey].forEach((item) => {
+      jsxContent.push(<TodoItem item={item}/>)
+    })
+  });
+
   return <div className="w-3/4">
-    { todoList.map((item) => {
-        return <TodoItem item={item}/>;
-  })}</div>
+    {jsxContent}
+  </div>
 }
 
 export default TodoList;
