edc706fa66a5f1676765d69da1389e91643fd7d2
nigel <>
2022-08-08 17:58:17 +0200

    adapt CalendarMonthDay to todoItemDetail date change function

--- src/components/calendar/CalendarMonthDay.js
+++ src/components/calendar/CalendarMonthDay.js
@@ -8,7 +8,7 @@ import TodoList from "../todo/TodoList";
 import useTodoMap from "../../hooks/useTodoMap";
 import TodoItemObj from "../../model/TodoItemObj";
 
-function CalendarMonthDay(props){
+function CalendarMonthDay(props) {
   let dayArr = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
   const [formModalActive, setFormModalActive] = useState(false)
   const [detailModalActive, setDetailModalActive] = useState(false)
@@ -38,19 +38,28 @@ function CalendarMonthDay(props){
     setDetailModalActive(false)
   }
 
+  const detailDateChangeHandler = () => {
+    setDetailModalActive(false);
+    setId(-1)
+  }
+
   return <div className="p-2 m-2 shadow rounded bg-blue-200">
-    <p className="mb-2">{dayArr[(props.date.getDay() + 6)%7]}</p>
+    <p className="mb-2">{dayArr[(props.date.getDay() + 6) % 7]}</p>
     <p className="mb-2">{CalendarUtils.formatDate(props.date)}</p>
     <TodoList
         todoList={todoList}
         onItemClick={todoClickHandler}
     />
     <button className="p-2 rounded bg-blue-500" onClick={formButtonCLickHandler}>+</button>
-    <Modal onClickOutside={formModalOutsideClickHandler} active={formModalActive} modalDivId="formModal" backdropDivId="formBackdrop" >
-      <TodoForm date={props.date} afterSubmit={formModalOutsideClickHandler} />
+    <Modal onClickOutside={formModalOutsideClickHandler} active={formModalActive} modalDivId="formModal"
+           backdropDivId="formBackdrop">
+      <TodoForm date={props.date} afterSubmit={formModalOutsideClickHandler}/>
     </Modal>
-    <Modal onClickOutside={detailModalOutsideClickHandler} active={detailModalActive} modalDivId="todoModal" backdropDivId="todoBackdrop" >
-      <TodoItemDetail afterDelete={detailModalOutsideClickHandler} item={id !== -1 ? todoMap[dateString][id] : null} />
+    <Modal onClickOutside={detailModalOutsideClickHandler} active={detailModalActive} modalDivId="todoModal"
+           backdropDivId="todoBackdrop">
+      <TodoItemDetail afterDelete={detailModalOutsideClickHandler}
+                      afterDateChange={detailDateChangeHandler}
+                      item={id !== -1 ? todoMap[dateString][id] : null}/>
     </Modal>
   </div>
 }
