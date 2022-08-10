1345aaa7d809c363d8f4cfc4acd2ff8e6a30a607
nigel <>
2022-08-10 22:52:37 +0200

    convert CalendarMonth to typescript

--- src/components/calendar/CalendarMonth.tsx
+++ src/components/calendar/CalendarMonth.tsx
@@ -1,22 +1,26 @@
-import CalendarUtils from "../../utils/CalendarUtils";
+
 import CalendarMonthDay from "./CalendarMonthDay";
 import {useState} from "react";
 import useTodoMap from "../../hooks/useTodoMap";
+import React from "react";
+import CalendarUtils from "../../utils/CalendarUtils";
 
-function CalendarMonth(){
-  const [selectedDate, setSelectedDate] = useState(new Date());
+
+
+const CalendarMonth: React.FC = () => {
+  const [selectedDate, setSelectedDate] = useState<Date | null>(new Date());
 
   const todoMap = useTodoMap();
 
-  let monthWeekList = CalendarUtils.getMonthWeeksList(selectedDate);
+  let monthWeekList = CalendarUtils.getMonthWeeksList(selectedDate === null ? new Date() : selectedDate);
 
-  const dateChangeHandler = (evt) => {
+  const dateChangeHandler = (evt: React.ChangeEvent<HTMLInputElement>) => {
     setSelectedDate(evt.target.valueAsDate)
   }
 
   return <div className="flex flex-col">
     <input type="date"
-           value={CalendarUtils.formatDateForInput(selectedDate)}
+           value={selectedDate === null ? undefined : CalendarUtils.formatDateForInput(selectedDate)}
            onChange={dateChangeHandler}/>
     {monthWeekList.map((week)=>{
       return <div className="flex flex-row">
