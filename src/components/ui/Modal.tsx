e2d2c79ee867e20408689f7688af202ee4084af2
nigel <>
2022-08-10 23:06:14 +0200

    convert Modal to typescript

---
+++ src/components/ui/Modal.tsx
@@ -0,0 +1,42 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+
+const Backdrop:React.FC<{onClick: ()=>void }> = (props) =>{
+  return <div onClick={props.onClick} className={`absolute w-screen h-screen opacity-70 bg-black z-40`}>
+  </div>
+}
+
+const ModalContent:React.FC<{children: React.ReactNode}> = (props) => {
+  return <div className={`p-2 fixed top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2 bg-white z-50`}>
+    {props.children}
+  </div>
+}
+
+type Props = {
+  active: boolean,
+  onClickOutside: ()=>void,
+  modalDivId: string,
+  backdropDivId: string,
+  children: React.ReactNode
+}
+
+const Modal: React.FC<Props> = (props) => {
+
+  return <React.Fragment>
+    {props.active &&
+        document.getElementById(props.backdropDivId) !== null &&
+        ReactDOM.createPortal(
+            <Backdrop onClick={props.onClickOutside}/>,
+            document.getElementById(props.backdropDivId)!
+        )};
+    {props.active &&
+        document.getElementById(props.modalDivId) !== null &&
+        ReactDOM.createPortal(
+            <ModalContent>{props.children}</ModalContent>,
+            document.getElementById(props.modalDivId)!
+        )};
+  </React.Fragment>
+}
+
+export default Modal;
