296d206525ac759e3ba1a5b55a2eb7ebc87e11f2
nigel <>
2022-08-11 23:38:44 +0200

    add syncstatus to todoitem model

--- src/model/TodoItemObj.tsx
+++ src/model/TodoItemObj.tsx
@@ -1,9 +1,17 @@
+export enum SyncStatus{
+  Pending,
+  Syncing,
+  Complete,
+  Failed
+}
+
 export type TodoItemDTO = {
   id: string
   title: string
   dateAsTime: number | null;
   description: string;
   done: boolean;
+  syncStatus: SyncStatus
 }
 
 class TodoItemObj {
@@ -12,23 +20,26 @@ class TodoItemObj {
   date: Date | null;
   description: string;
   done: boolean;
+  syncStatus: SyncStatus
 
   static get WITHOUT_DATE_IDENTIFIER() {
     return "no-date"
   }
 
 
-  constructor(id: string, title: string, date: Date | null, description: string, done=false) {
+  constructor(id: string, title: string, date: Date | null, description: string,
+              syncStatus: SyncStatus = SyncStatus.Pending, done=false) {
     this.id = id;
     this.title = title;
     this.date = date
     this.description = description;
+    this.syncStatus = syncStatus;
     this.done=done;
   }
 
   static fromDto(dto: TodoItemDTO){
     let date = dto.dateAsTime === null ? null : new Date(dto.dateAsTime)
-    return new TodoItemObj(dto.id, dto.title, date, dto.description, dto.done)
+    return new TodoItemObj(dto.id, dto.title, date, dto.description, dto.syncStatus, dto.done)
   }
 
   getDto = () => {
@@ -37,6 +48,7 @@ class TodoItemObj {
       title: this.title,
       dateAsTime: this.date === null ? null : this.date.getTime(),
       description: this.description,
+      syncStatus: this.syncStatus,
       done: this.done
     }
   }
