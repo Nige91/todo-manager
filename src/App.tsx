e0519d73e7447cc3e07d85fdb6457da7f3e586cd
nigel <nigel.nelles@googlemail.com>
2022-08-13 22:12:11 +0200

    update items in firebase when changed locally

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,24 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import './App.css';
 // @ts-ignore
 import Sidebar from "./components/sidebar/Sidebar";
 import RouterView from "./views/RouterView";
+import {useDispatch, useSelector} from "react-redux";
+import {RootState} from "./store/store";
+import {SyncStatus, TodoItemDTO} from "./model/TodoItemObj";
+import {syncTodo} from "./store/todoDictDtoSlice";
+import {ThunkDispatch} from "@reduxjs/toolkit";
 
 function App() {
+  const todoItemDTOList = useSelector<RootState, TodoItemDTO[]>(state => Object.values(state.todoDictDto));
+  const dispatch = useDispatch<ThunkDispatch<any, any, any>>();
+
+  useEffect(()=> {
+    todoItemDTOList.filter(item=>item.syncStatus === SyncStatus.Pending).forEach(item=>{
+      dispatch(syncTodo(item));
+    })
+  }, [todoItemDTOList, dispatch])
+
   return (
       <div className="flex flex-row">
         <Sidebar />
