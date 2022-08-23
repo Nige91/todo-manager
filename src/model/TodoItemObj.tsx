f40e23f82cd5eedd54591a7b971623a4f929f95d
nigel <nigel.nelles@googlemail.com>
2022-08-24 01:11:59 +0200

    add syncAction to todoItem model

--- src/model/TodoItemObj.tsx
+++ src/model/TodoItemObj.tsx
@@ -5,6 +5,11 @@ export enum SyncStatus{
   FAILED
 }
 
+export enum SyncAction{
+  ADD_OR_UPDATE,
+  DELETE
+}
+
 export type TodoItemDTO = {
   id: string
   title: string
@@ -12,6 +17,7 @@ export type TodoItemDTO = {
   description: string;
   done: boolean;
   syncStatus: SyncStatus
+  syncAction: SyncAction
 }
 
 class TodoItemObj {
@@ -21,6 +27,7 @@ class TodoItemObj {
   description: string;
   done: boolean;
   syncStatus: SyncStatus
+  syncAction: SyncAction
 
   static get WITHOUT_DATE_IDENTIFIER() {
     return "no-date"
@@ -28,18 +35,21 @@ class TodoItemObj {
 
 
   constructor(id: string, title: string, date: Date | null, description: string,
-              syncStatus: SyncStatus = SyncStatus.PENDING, done=false) {
+              syncStatus: SyncStatus = SyncStatus.PENDING,
+              syncAction: SyncAction = SyncAction.ADD_OR_UPDATE,
+              done=false) {
     this.id = id;
     this.title = title;
     this.date = date
     this.description = description;
     this.syncStatus = syncStatus;
+    this.syncAction = syncAction;
     this.done=done;
   }
 
   static fromDto(dto: TodoItemDTO){
     let date = dto.dateAsTime === null ? null : new Date(dto.dateAsTime)
-    return new TodoItemObj(dto.id, dto.title, date, dto.description, dto.syncStatus, dto.done)
+    return new TodoItemObj(dto.id, dto.title, date, dto.description, dto.syncStatus, dto.syncAction, dto.done)
   }
 
   getDto = () => {
@@ -49,6 +59,7 @@ class TodoItemObj {
       dateAsTime: this.date === null ? null : this.date.getTime(),
       description: this.description,
       syncStatus: this.syncStatus,
+      syncAction: this.syncAction,
       done: this.done
     }
   }
