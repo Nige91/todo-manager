683fe2b5535f7b0d8ceb9fbbc4e728843317c4b7
nigel <nigel.nelles@googlemail.com>
2022-08-28 00:23:01 +0200

    add modal animation

--- src/components/ui/Modal.tsx
+++ src/components/ui/Modal.tsx
@@ -3,12 +3,12 @@ import ReactDOM from "react-dom"
 
 
 const Backdrop:React.FC<{onClick: ()=>void }> = (props) =>{
-  return <div onClick={props.onClick} className={`absolute w-screen h-screen opacity-70 bg-black z-40`}>
+  return <div onClick={props.onClick} className={`absolute w-screen h-screen opacity-70 bg-black z-40 modal-anim-bg`}>
   </div>
 }
 
 const ModalContent:React.FC<{children: React.ReactNode}> = (props) => {
-  return <div className={`p-2 fixed top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2 bg-white z-50`}>
+  return <div className={`p-2 fixed top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2 bg-white z-50 modal-anim-content origin-center`}>
     {props.children}
   </div>
 }
