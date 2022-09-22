f289815a2beeb18fc9383206fbeea8e3e53046cd
nigel <nigel.nelles@googlemail.com>
2022-09-22 07:44:42 +0200

    define colors for theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,13 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
+    colors: {
+      'clr-dark': '#066B76',
+      'clr-light': '#6AACB3',
+      'clr-accent': '#F79A21',
+      'neutral-light': '#CCCCCC',
+      'neutral-dark': '#454545',
+    },
     extend: {},
   },
   plugins: [require("daisyui")],
