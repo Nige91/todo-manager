8c0d2a73641d5a84d724dc0641225047bda61d16
nigel <nigel.nelles@googlemail.com>
2022-11-22 12:44:26 +0100

    add loading spinner component for app startup use the spinner for

--- src/components/todo/TodoItemSmall.tsx
+++ src/components/todo/TodoItemSmall.tsx
@@ -1,5 +1,6 @@
 import React from "react"
 import TodoItemObj, {SyncAction, SyncStatus} from "../../model/TodoItemObj"
+import Spinner from "../ui/Spinner";
 
 type Props = {
   onClick: (id: string) => void
@@ -10,9 +11,9 @@ const TodoItemSmall: React.FC<Props> = (props) => {
   const color = props.item.done ? "bg-clr-done" : "bg-clr-ndone"
   const opacity = props.item.syncAction === SyncAction.DELETE  ? "opacity-40" : ""
 
-  return <p onClick={() => props.onClick(props.item.id)} className={`p-2 mb-2 ${color} ${opacity} rounded`} >
-    {props.item.title} {props.item.syncStatus===SyncStatus.SYNCING &&
-      <img src={process.env.PUBLIC_URL + '/loading.gif'} className="w-2 h-2" alt="sync in progress"/>}
+  return <p onClick={() => props.onClick(props.item.id)} className={`p-2 mb-2 ${color} ${opacity} rounded flex`} >
+    {props.item.title}
+    <Spinner visible={props.item.syncStatus===SyncStatus.SYNCING} size={2} styles="relative ml-auto mr-1"/>
   </p>
 }
 
